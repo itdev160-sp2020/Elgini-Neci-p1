bfc1fe4c558a1805adde203d85da5784a0adceea
Elgin Neci <elginneci@elgins-air.home>
2020-03-17 22:53:10 -0500

    Completed Project1

---
+++ Java/java.js
@@ -0,0 +1,16 @@
+var averageprice;
+var stars;
+var welcomemessage;
+var greeting=document.getElementById("greetings");
+greeting.textContent=welcomemessage;
+var cupon=0.1;
+var newprice;
+
+//welcome
+welcomemessage='This is the best two restaurants in Milwaukee right now!';
+starts=5;
+
+
+//price
+averageprice="$95";
+newprice=averageprice-(averageprice*cupon);
--- README.md
+++
@@ -1,1 +0,0 @@
-# Elgini-Neci-p1
